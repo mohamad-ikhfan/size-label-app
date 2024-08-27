2eaab2377a7e9b229d606b6c70a50879209de199
mohamad-ikhfan <mohamad.ikhfan@gmail.com>
2024-08-27 07:58:18 +0700

    upload

--- resources/js/Pages/SchedulePrints/Generate.jsx
+++ resources/js/Pages/SchedulePrints/Generate.jsx
@@ -1,9 +1,7 @@
-import Checkbox from "@/Components/Checkbox";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import SecondaryButton from "@/Components/SecondaryButton";
-import SelectInput from "@/Components/SelectInput";
 import TextInput from "@/Components/TextInput";
 import { useForm } from "@inertiajs/react";
 import toast from "react-hot-toast";
