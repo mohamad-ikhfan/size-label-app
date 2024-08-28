2de700af2be8c610190058edc2cb355b27a0ac55
mohamad-ikhfan <mohamad.ikhfan@gmail.com>
2024-08-28 13:11:05 +0700

    upload

---
+++ resources/js/Components/Notification.jsx
@@ -0,0 +1,111 @@
+import { BellIcon } from "@heroicons/react/16/solid";
+import Dropdown from "./Dropdown";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { router } from "@inertiajs/react";
+
+export default function Notification() {
+    const [notification, setNotification] = useState([]);
+    const [unreadNotification, setUnreadNotification] = useState(0);
+
+    const getNotification = async () => {
+        await axios.get(route("notification.index")).then((response) => {
+            setNotification(response.data);
+            setUnreadNotification(
+                response.data.filter((v) => v.read_at === null).length
+            );
+        });
+    };
+
+    const handleReadNotification = (id) => {
+        router.put(route("notification.read", id), [], {
+            onSuccess: () => getNotification(),
+        });
+    };
+
+    const handleDeleteNotification = (id) => {
+        router
+            .delete(route("notification.destroy", id))
+            .then(() => getNotification());
+    };
+
+    useEffect(() => {
+        let timer = setTimeout(() => getNotification(), 5000);
+
+        () => clearTimeout(timer);
+    });
+
+    return (
+        <Dropdown>
+            <Dropdown.Trigger>
+                <span className="inline-flex rounded-md">
+                    <button
+                        type="button"
+                        className="inline-flex items-center p-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                    >
+                        <BellIcon className="w-5" />
+                        {unreadNotification > 0 && (
+                            <span className="absolute top-0 right-0 px-1 rounded-full bg-orange-600 text-orange-50 text-xs">
+                                {unreadNotification}
+                            </span>
+                        )}
+                    </button>
+                </span>
+            </Dropdown.Trigger>
+
+            <Dropdown.Content>
+                <div className="px-2 py-4 text-sm max-h-96 overflow-y-auto space-y-1">
+                    {notification.length > 0 ? (
+                        notification.map((notif) => (
+                            <div
+                                className={
+                                    notif.read_at === null
+                                        ? "bg-green-200 p-2 rounded-md space-y-1"
+                                        : "bg-gray-200 p-2 rounded-md space-y-1"
+                                }
+                                key={notif.id}
+                            >
+                                <h6
+                                    className={
+                                        notif.data.notif === "success"
+                                            ? "font-semibold text-green-800"
+                                            : "font-semibold text-red-800"
+                                    }
+                                >
+                                    {notif.data.title}
+                                </h6>
+                                <p className="text-wrap text-gray-900">
+                                    {notif.data.message}
+                                </p>
+                                <div className="flex flex-wrap justify-between gap-2">
+                                    <button
+                                        type="button"
+                                        className="text-xs text-blue-800 text-nowrap"
+                                        onClick={(e) =>
+                                            handleReadNotification(notif.id)
+                                        }
+                                    >
+                                        Mark as read
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="text-xs text-red-800 text-nowrap"
+                                        onClick={(e) =>
+                                            handleDeleteNotification(notif.id)
+                                        }
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="text-center dark:text-gray-400">
+                            No notification.
+                        </div>
+                    )}
+                </div>
+            </Dropdown.Content>
+        </Dropdown>
+    );
+}
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,47 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
-import { Link, router } from "@inertiajs/react";
+import { Link } from "@inertiajs/react";
 import { Toaster } from "react-hot-toast";
-import { ChevronUpDownIcon, BellIcon } from "@heroicons/react/16/solid";
-import axios from "axios";
+import { ChevronUpDownIcon } from "@heroicons/react/16/solid";
+import Notification from "@/Components/Notification";
 
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
-
-    const [notification, setNotification] = useState([]);
-    const [unreadNotification, setUnreadNotification] = useState(0);
-
-    const getNotification = () => {
-        axios
-            .get(route("notification.index"))
-            .then((response) => {
-                setNotification(response.data);
-                setUnreadNotification(
-                    response.data.filter((v) => v.read_at === null).length
-                );
-            })
-            .catch(() => router.reload());
-    };
-
-    const handleReadNotification = (id) => {
-        router.put(route("notification.read", id));
-    };
-
-    const handleDeleteNotification = (id) => {
-        router.delete(route("notification.destroy", id));
-        getNotification();
-    };
-
-    useEffect(() => {
-        let timer = setTimeout(() => getNotification(), 10000);
-
-        () => clearTimeout(timer);
-    });
-
     return (
         <>
             <Toaster />
@@ -151,100 +120,7 @@ export default function Authenticated({ user, header, children }) {
 
                             <div className="hidden sm:flex sm:items-center sm:ms-6">
                                 <div className="ms-3 relative p-2">
-                                    <Dropdown>
-                                        <Dropdown.Trigger>
-                                            <span className="inline-flex rounded-md">
-                                                <button
-                                                    type="button"
-                                                    className="inline-flex items-center p-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
-                                                >
-                                                    <BellIcon className="w-5" />
-                                                    {unreadNotification > 0 && (
-                                                        <span className="absolute top-0 right-0 px-1 rounded-full bg-orange-600 text-orange-50 text-xs">
-                                                            {unreadNotification}
-                                                        </span>
-                                                    )}
-                                                </button>
-                                            </span>
-                                        </Dropdown.Trigger>
-
-                                        <Dropdown.Content>
-                                            <div className="px-2 py-4 text-sm max-h-96 overflow-y-auto space-y-1">
-                                                {notification.length > 0 ? (
-                                                    notification.map(
-                                                        (notif) => (
-                                                            <div
-                                                                className={
-                                                                    notif.read_at ===
-                                                                    null
-                                                                        ? "bg-green-200 p-2 rounded-md space-y-1"
-                                                                        : "bg-gray-200 p-2 rounded-md space-y-1"
-                                                                }
-                                                                key={notif.id}
-                                                            >
-                                                                <h6
-                                                                    className={
-                                                                        notif
-                                                                            .data
-                                                                            .notif ===
-                                                                        "success"
-                                                                            ? "font-semibold text-green-800"
-                                                                            : "font-semibold text-red-800"
-                                                                    }
-                                                                >
-                                                                    {
-                                                                        notif
-                                                                            .data
-                                                                            .title
-                                                                    }
-                                                                </h6>
-                                                                <p className="text-wrap text-gray-900">
-                                                                    {
-                                                                        notif
-                                                                            .data
-                                                                            .message
-                                                                    }
-                                                                </p>
-                                                                <div className="flex flex-wrap justify-between gap-2">
-                                                                    <button
-                                                                        type="button"
-                                                                        className="text-xs text-blue-800 text-nowrap"
-                                                                        onClick={(
-                                                                            e
-                                                                        ) =>
-                                                                            handleReadNotification(
-                                                                                notif.id
-                                                                            )
-                                                                        }
-                                                                    >
-                                                                        Mark as
-                                                                        read
-                                                                    </button>
-                                                                    <button
-                                                                        type="button"
-                                                                        className="text-xs text-red-800 text-nowrap"
-                                                                        onClick={(
-                                                                            e
-                                                                        ) =>
-                                                                            handleDeleteNotification(
-                                                                                notif.id
-                                                                            )
-                                                                        }
-                                                                    >
-                                                                        Delete
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        )
-                                                    )
-                                                ) : (
-                                                    <div className="text-center dark:text-gray-400">
-                                                        No notification.
-                                                    </div>
-                                                )}
-                                            </div>
-                                        </Dropdown.Content>
-                                    </Dropdown>
+                                    <Notification />
                                 </div>
                                 <div className="relative">
                                     <Dropdown>
